eba3ada629d5b2044d28980f34a9360275b0cc92
Daniel Zenke <danielzenke94@gmail.com>
2026-03-05 00:26:01 +0100

    fix: revert to normal path

--- files/scripts/install-sddm-theme.sh
+++ files/scripts/install-sddm-theme.sh
@@ -8,29 +8,22 @@ dnf install -y \
     qt6-qtmultimedia
 
 # 2. Define the paths
-WRITABLE_PATH="/usr/local/share/sddm/themes/sddm-astronaut-theme"
-SYSTEM_PATH="/usr/share/sddm/themes/sddm-astronaut-theme"
+THEME_PATH="/usr/share/sddm/themes/sddm-astronaut-theme"
 
 # 3. Create the writable directory and clone
 mkdir -p "/usr/local/share/sddm/themes"
-if [ ! -d "$WRITABLE_PATH" ]; then
     git clone -b master --depth 1 \
         https://github.com/Keyitdev/sddm-astronaut-theme.git \
-        "$WRITABLE_PATH"
+            "$THEME_PATH"
 fi
 
 # 4. Select pixel_sakura variant in the WRITABLE path
 sed -i 's|^ConfigFile=.*|ConfigFile=Themes/pixel_sakura.conf|' \
-    "$WRITABLE_PATH/metadata.desktop"
-
-# 5. THE TRICK: Create the symlink
-# We link the standard path to our writable path
-mkdir -p /usr/share/sddm/themes
-ln -s "$WRITABLE_PATH" "$SYSTEM_PATH"
+    "$THEME_PATH/metadata.desktop"
 
 # 6. Fonts (Keep these in /usr/share for reliability)
 mkdir -p /usr/share/fonts/sddm-astronaut
-cp -r "$WRITABLE_PATH/Fonts/"* /usr/share/fonts/sddm-astronaut/
+cp -r "$THEME_PATH/Fonts/"* /usr/share/fonts/sddm-astronaut/
 
 # 7. Standard Config (No ThemeDir needed now!)
 mkdir -p /etc/sddm.conf.d
